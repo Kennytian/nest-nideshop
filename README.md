77766d744be0cfa396e6d8a713af6a699e5aeabf
Kennytian <kt.tian@gmail.com>
2020-04-25 12:55:00 +0800

    feat: Add untracked files

---
+++ README.md
@@ -0,0 +1,14 @@
+# nest-nideshop
+
+
+#### nest help
+- nest --help
+
+#### generator service
+- nest g s feature/cats
+
+#### generator controller
+- nest g co feature/cats
+
+#### generator module
+- nest g mo feature/cats
---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MyLogger } from './utils/my-logger';
+import { setupSwagger } from './shared/swagger';
+
+async function bootstrap() {
+  const host = process.env.NODE_HOST || '127.0.0.1';
+  console.log('process.env.NODE_PORT====', process.env.NODE_PORT);
+  const port = process.env.NODE_PORT || 3005;
+
+  const app = await NestFactory.create(AppModule, {
+    // logger: ['warn','error'],
+    logger: new MyLogger(),
+  });
+
+  if (process.env.NODE_ENV === 'development') {
+    setupSwagger(app, `${host}:${port}`);
+  }
+
+  await app.listen(port, host);
+
+  console.log('process.env.NODE_ENV======', process.env.NODE_ENV);
+
+  console.log(`Application is running on: http://${host}:${port}/cats`);
+}
+bootstrap();
